2bdc2d39ba5e146a9038817f618823ec8e6fbc00
lemoniapd <lemonia.pd@gmail.com>
2023-04-24 10:33:20 +0200

    Initial commit

---
+++ src/main/java/com/example/restapiwebshopgroupproj/ResTapiWebshopGroupProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiwebshopgroupproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTapiWebshopGroupProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTapiWebshopGroupProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapiwebshopgroupproj/ResTapiWebshopGroupProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapiwebshopgroupproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTapiWebshopGroupProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
