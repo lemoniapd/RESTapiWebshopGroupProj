76fc7f9138282b7a3f7aef3fb84db722a304ed61
Tallgren <toby9847@gmail.com>
2023-04-26 14:17:29 +0200

    Changes by peter

--- src/main/java/com/example/restapiwebshopgroupproj/Models/Orders.java
+++ src/main/java/com/example/restapiwebshopgroupproj/Models/Orders.java
@@ -1,5 +1,6 @@
 package com.example.restapiwebshopgroupproj.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
