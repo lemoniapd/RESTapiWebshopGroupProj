7b11cb734bdd510c297a6a497997cba8f28b4f34
lisapel <lisa.peldan@gmail.com>
2023-04-30 14:45:09 +0200

    Changes by Toby9

--- src/main/java/com/example/restapiwebshopgroupproj/Models/Orders.java
+++ src/main/java/com/example/restapiwebshopgroupproj/Models/Orders.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -29,19 +30,13 @@ public class Orders {
     @ManyToMany (fetch = FetchType.EAGER)
     protected List<Product> products;
 
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        return format.format(date);
+    }
     public Orders(Date date, Customer customer, List<Product> products) {
         this.date = date;
         this.customer = customer;
         this.products = products;
     }
-    */
-
-
-
-    public Orders(Date date, Customer customer, List<Product> products) {
-        this.date = date;
-        this.customer = customer;
-        this.products = products;
-    }
-
 }
