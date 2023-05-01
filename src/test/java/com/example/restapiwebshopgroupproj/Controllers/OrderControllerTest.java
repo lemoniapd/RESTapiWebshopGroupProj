c78f7002d972ebac5125bac324213bd9455d2a8b
Tallgren <toby9847@gmail.com>
2023-05-01 11:36:00 +0200

    Changes by peter

--- src/test/java/com/example/restapiwebshopgroupproj/Controllers/OrderControllerTest.java
+++ src/test/java/com/example/restapiwebshopgroupproj/Controllers/OrderControllerTest.java
@@ -4,6 +4,7 @@ import com.example.restapiwebshopgroupproj.Models.Customer;
 import com.example.restapiwebshopgroupproj.Models.Orders;
 import com.example.restapiwebshopgroupproj.Models.Product;
 import com.example.restapiwebshopgroupproj.Repositories.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,9 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -35,21 +39,24 @@ class OrderControllerTest {
     public void init() {
         Customer c1 = new Customer(1L, "Peter", "1234567");
         Product p1 = new Product(1L, 10.9, "Sytråd röd");
-
-        Orders o1 = new Orders(1L, new Date(), c1, List.of(p1));
-        Orders o2 = new Orders(1L, new Date(), c1, List.of(p1));
-        Orders o3 = new Orders(1L, new Date(), c1, List.of(p1));
-        when(customerRepo.findById(1L)).thenReturn(Optional.of(c1));
-        when(customerRepo.findById(2L)).thenReturn(Optional.of(c2));
-        when(customerRepo.findById(3L)).thenReturn(Optional.of(c3));
-        when(customerRepo.findAll()).thenReturn(Arrays.asList(c1, c2, c3));
-
+        Orders o1 = new Orders(1L, new Date(2023 - 05 - 01), c1, List.of(p1));
+        Orders o2 = new Orders(1L, new Date(2023 - 05 - 01), c1, List.of(p1));
+        Orders o3 = new Orders(1L, new Date(2023 - 05 - 01), c1, List.of(p1));
+        when(customerRepo.findById(1L)).thenReturn(Optional.of(o1));
+        when(customerRepo.findById(2L)).thenReturn(Optional.of(o2));
+        when(customerRepo.findById(3L)).thenReturn(Optional.of(o3));
+        when(customerRepo.findAll()).thenReturn(Arrays.asList(o1, o2, o3));
     }
 
-     */
-
     @Test
-    void getAllOrders() {
+    void getAllOrders() throws Exception {
+        this.mockMvc.perform(get("/orders/"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        new ObjectMapper()
+                                .writeValueAsString()
+
+
     }
 
     @Test
